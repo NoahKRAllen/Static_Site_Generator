2833f342b5f970a69e5a406b6b13740ca9bdf651
Noah <noahkrallen@gmail.com>
2026-06-24 20:48:51 -0700

    All is looking good, attempting to deploy

--- src/main.py
+++ src/main.py
@@ -3,10 +3,11 @@ from inline import extract_title, markdown_to_html_node
 
 import shutil
 import os
+import pathlib
 def main():
 	delete_remake_public_dir('public')
 	copy_dir_to_dir('static', 'public')
-	generate_page("content/index.md", "template.html", "public/index.html")
+	generate_pages_recursive("content", "template.html", "public")
 
 
 def delete_remake_public_dir(dir_path):
@@ -43,6 +44,18 @@ def generate_page(from_path, template_path, dest_path):
 	with open(dest_path, 'w', encoding="utf-8") as f:
 		f.write(template_updated)
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+	dir_list = os.listdir(dir_path_content)
+	for item in dir_list:
+		from_path = os.path.join(dir_path_content, item)
+		dest_path = os.path.join(dest_dir_path, item)
+		if os.path.isfile(from_path):
+			path = pathlib.Path(dest_path)
+			dest_path = path.with_suffix(".html")
+			generate_page(from_path, template_path, dest_path)
+		else:
+			generate_pages_recursive(from_path, template_path, dest_path)
 
 if __name__ == "__main__":
 	main()
+
